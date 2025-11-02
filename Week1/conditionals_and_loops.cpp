a4da2e9615d2c4efd35e1770bc6ce520f05853ff
Yashmagarde <yashmagarde1@gmail.com>
2025-11-02 07:48:57 +0530

    week 1 conditionals and loops code updated

--- Week1/conditionals_and_loops.cpp
+++ Week1/conditionals_and_loops.cpp
@@ -2,6 +2,43 @@
 using namespace std;
 
 int main(){
+    int n;
+    cin>>n;
+
+    //:::if else statements:::
+
+    // if(n>0){
+    //     cout<<"positive";
+    // }
+    // else if(n<0){
+    //     cout<<"negative";
+    // }
+    // else{
+    //     cout<<"zero";
+    // }
+
+
+    // if(n%2 == 0){
+    //     cout<<"even";
+    // }
+    // else{
+    //     cout<<"odd";
+    // }
+
+
+
+    //:::bonus questions:::
+
+    // if(cin>>n){
+    //     cout<<n;
+    // }
+
+    // if(cout<<"name"){
+    //     cout<<"yash";
+    // }
+
+
+    //:::pattern printing:::
 
     //square
     // for(int i =0;i<4;i++){
@@ -9,12 +46,26 @@ int main(){
     //         cout<<"* ";
     //     }
     //     cout<<endl;
-    // }J
+    // }
+
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
 
 
     //rectangle
     // for(int i=0;i<3;i=i+1){
     //     for(int i=0;i<5;i=i+1){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<2*n;j++){
     //         cout<<"* ";
     //     }
     //     cout<<endl;
@@ -38,9 +89,20 @@ int main(){
     //     cout<<endl;
     // }
 
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<2*n;j++){
+    //         if(i==0 || i==n-1 || j==0 || j==(2*n)-1){
+    //             cout<<"* ";
+    //         }
+    //         else{    
+    //            cout<<"  ";    
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
     //half pyramid
-    int n;
-    cin>>n;
+    
     // for (int i = 0; i < n; i++)
     // {
     //     for(int j=0;j<i+1;j++){
@@ -49,7 +111,22 @@ int main(){
     //      cout<<endl;
     // }
 
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<i+1;j++){
+    //         cout<<"* ";
+    //     }
+
+    //     cout<<endl;
+    // }
+
     //inverted half pyramid
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n-i;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
     // for(int i=0;i<n;i++){
     //     for(int j=0;j<n-i;j++){
     //         cout<<"* ";
@@ -66,6 +143,13 @@ int main(){
     //     cout<<endl;
     // }
 
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<i+1;j++){
+    //         cout<<j+1<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
     //inverted numeric half pyramid
     // for(int i=0;i<n;i++){
     //     for(int j=0;j<n-i;j++){
@@ -74,10 +158,14 @@ int main(){
     //     }
     //     cout<<endl;
     // }
+
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n-i;j++){
+    //         cout<<j+1<<" ";
+    //     }
+    //     cout<<endl;
+    // }
     
-
-
-
 
     return 0;
 }
