196a37ef24db8737187ad09fd2d43de3d6aa488d
Yashmagarde <yashmagarde1@gmail.com>
2025-11-03 08:18:17 +0530

    continue break statement and variable scoping added

--- week2/biwise_operator_and_loops.cpp
+++ week2/biwise_operator_and_loops.cpp
@@ -21,6 +21,33 @@ int main(){
     // cout<<(a>>-1);
 
 
+    // continue break statement
+
+    // for(int i=0;i<5;i++){
+    //     if(i==1){
+    //         break;
+    //     }
+    //     cout<<i;
+    // }
+
+    // for(int i=0;i<5;i++){
+    //     if(i==2){
+    //         continue;
+    //     }
+    //     cout<<i<<endl;
+    // }
+
+
+    //Variable Scoping
+
+    //local variable
+    // for(int i=0;i<3;i++){
+    //     cout<<i<<endl;
+    // }
+
+    // cout<<"or bhai i kya value hai teri"<<i<<endl;
+
+    //global variable(bad practice)
 
 
 
