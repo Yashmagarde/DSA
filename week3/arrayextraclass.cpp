1bb8fd333b69340a38e2fa9efbe4b94b4a4ca139
Yashmagarde <yashmagarde1@gmail.com>
2025-11-06 12:28:43 +0530

    array extra class updated

--- week3/arrayextraclass.cpp
+++ week3/arrayextraclass.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 void sortnegpos(int arr[],int size){
@@ -12,13 +13,46 @@ void sortnegpos(int arr[],int size){
 
 }
 
+void sortcolors(vector<int>&nums){
+    int left=0;
+    int right = nums.size()-1;
+    int index =0;
+    while(index<=right){
+        if(nums[index]==0){
+            swap(nums[index],nums[left]);
+            left++;
+            index++;
+        }
+        else if(nums[index]==2){
+            swap(nums[index],nums[right]);
+                right--;
+        }
+        else{
+            index++;
+        }
+    }
+}
+
+
 int main(){
-    int arr[7] = {23,-7,12,-10,-11,40,60};
-    int size = 7;
 
-    sortnegpos(arr,size);
+    //Sort negative positive
 
-    for(int i=0;i<size;i++){
-        cout<<arr[i]<<" ";
+    // int arr[7] = {23,-7,12,-10,-11,40,60};
+    // int size = 7;
+
+    // sortnegpos(arr,size);
+
+    // for(int i=0;i<size;i++){
+    //     cout<<arr[i]<<" ";
+    // }
+
+
+    //Leetcode 75 - Sort Colors;
+
+    vector<int>nums ={2,0,2,1,1,0};
+    sortcolors(nums);
+    for(int i=0;i<nums.size();i++){
+        cout<<nums[i]<<" ";
     }
 }
