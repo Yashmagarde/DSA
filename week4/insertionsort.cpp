b0c3944f9668aa22040f55e1dca6aaa1ec441efa
Yashmagarde <yashmagarde1@gmail.com>
2025-11-10 12:13:58 +0530

    insertion sort updated

--- week4/insertionsort.cpp
+++ week4/insertionsort.cpp
@@ -7,13 +7,14 @@ void insertionsort(vector<int>&arr){
     for(int i=1;i<n;i++){
         int key=arr[i];
         int j=i-1;
-        while(j>=0 && arr[j]>key){
+        while(j>=0 && arr[j]>key){  //if we use < this sign than array will be sorted in decreasing order
             arr[j+1]=arr[j];
             j--;
         }
         arr[j+1]=key;
     }
 }
+
 
 void print(vector<int>&arr){
     int n=arr.size();
